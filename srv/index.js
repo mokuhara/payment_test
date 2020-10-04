012e17c968f6333a14165904107b427ee426cb14
MotokiKasaharaBansuke <motoki.kasahara@plaid.co.jp>
2020-10-04 19:38:23 +0900

    mongodb保存機能追加

--- srv/index.js
+++ srv/index.js
@@ -1,6 +1,25 @@
 import express from "express";
 // import socketIO from "socket.io";
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const mongoose = require("mongoose");
+
+const UserList = require("./models/user");
+const MONGO_CONFIG = require("./config/mongodb");
+
+const connectOption = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+//Set up default mongoose connection
+mongoose.connect(MONGO_CONFIG.ENDPOINT, connectOption);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 export default (app, http) => {
   app.use(express.json());
@@ -8,6 +27,42 @@ export default (app, http) => {
 
   app.post("/api/v1/auth", (req, res) => {
     console.log(req.body);
+    const userId = new mongoose.Types.ObjectId();
+    const email = req.body.user.email;
+    // const hashedPassword = bcrypt.hashSync(req.body.password, saltRounds);
+    const status = "preInterview";
+    bcrypt.hash(req.body.user.password, saltRounds, (err, hashedPassword) => {
+      console.log(hashedPassword);
+      const filter = {
+        email,
+      };
+      const update = {
+        userId,
+        email,
+        password: hashedPassword,
+        status,
+        name: "",
+        iconUrl: "",
+        tag: "",
+        description: "",
+      };
+      UserList.updateOne(
+        filter,
+        update,
+        {
+          upsert: true,
+        },
+        (err, result) => {
+          if (!err) {
+            return res.json({
+              status: "success",
+            });
+          }
+          console.error(err);
+        }
+      );
+    });
+
     // res.send('Got a POST request');
   });
 
---
+++ srv/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UsersSchema = Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    status: String,
+    name: String,
+    email: String,
+    password: String,
+    iconUrl: String,
+    tag: String,
+    description: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+}, {
+    collection: "users"
+});
+module.exports = mongoose.model("UserList", UsersSchema);
